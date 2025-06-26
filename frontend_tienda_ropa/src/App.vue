6124c6f7c0daef63fde108365c6dbb9576ad8247
nataly tosube <natalytosubepinto06042002@gmail.com>
2025-06-26 14:07:39 -0400

    tiporafias

--- frontend_tienda_ropa/src/App.vue
+++ frontend_tienda_ropa/src/App.vue
@@ -1,62 +1,28 @@
+<template>
+  <Toast position="top-right" />
+  <GlobalNotifications />
+  <router-view />
+</template>
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { useAuthStore } from '@/stores/index'
-const authStore = useAuthStore()
+import { Toast } from 'primevue';
+import GlobalNotifications from '@/components/GlobalNotifications.vue';
 </script>
 
-<template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
-    <div class="container-fluid">
-      <a class="navbar-brand d-flex align-items-center" href="#">
-        <img alt="Vue logo" src="@/assets/logo.svg" width="40" height="40" class="me-2" />
-        Fashion Store
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item"><RouterLink class="nav-link" to="/">Home</RouterLink></li>
-          <li class="nav-item"><RouterLink class="nav-link" to="/cliente">Clientes</RouterLink></li>
-          <li class="nav-item">
-            <RouterLink class="nav-link" to="/producto">Productos</RouterLink>
-          </li>
-          <li class="nav-item"><RouterLink class="nav-link" to="/venta">Ventas</RouterLink></li>
 
-          <li class="nav-item">
-            <RouterLink class="nav-link" to="/categoria">Categoria</RouterLink>
-          </li>
-          <li class="nav-item">
-            <RouterLink class="nav-link" to="/empleado">Empleado</RouterLink>
-          </li>
-          <li class="nav-item"><RouterLink class="nav-link" to="/usuario">Usuario</RouterLink></li>
-          <li class="nav-item" v-if="authStore.token">
-            <a class="nav-link" href="#" @click.prevent="authStore.logout()">Cerrar Sesión</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <main class="container my-4">
-    <HelloWorld msg="¡Bienvenido a Fashion Store!" />
-    <RouterView />
-  </main>
-</template>
-
-<style scoped>
-/* Resalta el enlace activo */
-.nav-link.router-link-exact-active {
-  background: #fff !important;
-  color: #0d6efd !important;
-  border-radius: 0.25rem;
-}
+<style>
+@import '@fortawesome/fontawesome-free/css/all.css';
+@import '@/assets/vendor/bootstrap/css/bootstrap.min.css';
+@import '@/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
+@import '@/assets/fonts/iconic/css/material-design-iconic-font.min.css';
+@import '@/assets/fonts/linearicons-v1.0.0/icon-font.min.css';
+@import '@/assets/vendor/animate/animate.css';
+@import '@/assets/vendor/css-hamburgers/hamburgers.min.css';
+@import '@/assets/vendor/animsition/css/animsition.min.css';
+@import '@/assets/vendor/select2/select2.min.css';
+@import '@/assets/vendor/daterangepicker/daterangepicker.css';
+@import '@/assets/vendor/slick/slick.css';
+@import '@/assets/vendor/MagnificPopup/magnific-popup.css';
+@import '@/assets/vendor/perfect-scrollbar/perfect-scrollbar.css';
+@import '@/assets/css/util.css';
+@import '@/assets/css/main.css';
 </style>
